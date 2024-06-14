dbf6cb33d4a747a772c00ec5b18d83216cf35267
darkchoco <darkchoco@cloudscape.kr>
2024-06-14 14:37:12 +0200

    [lrn] counter pjt, useEffect added.

--- learn-react-next/counter/src/App.jsx
+++ learn-react-next/counter/src/App.jsx
@@ -1,18 +1,34 @@
 import './App.css';
 import Viewer from './components/Viewer.jsx';
 import Controller from './components/Controller.jsx';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    console.log(`텍스트: ${text}`);
+  }, [text]);
+
+  useEffect(() => {
+    console.log(`카운트: ${count}`);
+  }, [count]);
 
   const onClickButton = (value) => {
     setCount(count + value);
   };
 
+  const onChangeText = (e) => {
+    setText(e.target.value);
+  }
+  
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input value={text} onChange={onChangeText} />
+      </section>
       <section>
         <Viewer count={count} />
       </section>
