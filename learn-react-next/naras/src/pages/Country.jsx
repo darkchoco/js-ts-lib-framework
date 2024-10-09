e0af13f0de490a67f58bda6eb41e40e24c196208
darkchoco <darkchoco.ice@gmail.com>
2024-10-09 22:31:54 +0200

    [lrn] naras pjt, Country page implemented.

--- learn-react-next/naras/src/pages/Country.jsx
+++ learn-react-next/naras/src/pages/Country.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { fetchCountry } from '../api.js';
 import { useEffect, useState } from 'react';
+import style from './Country.module.css';
 
 export default function Country() {
   const params = useParams();
@@ -15,5 +16,31 @@ export default function Country() {
     setInitData();
   }, [params.code]);
 
-  return <div>Country : { params.code }</div>;
+  return (
+    <div className={ style.container }>
+      <div className={ style.header }>
+        <div className={ style.commonName }>
+          { country.flagEmoji }&nbsp;{ country.commonName }
+        </div>
+        <div className={ style.officialName }>
+          { country.officialName }
+        </div>
+      </div>
+      <img src={ country.flagImg } alt={ `${ country.commonName }의 국기 이미지입니다` }/>
+      <div className={ style.body }>
+        <div>
+          <b>코드 : </b>&nbsp;{ country.code }
+        </div>
+        <div>
+          <b>수도 : </b>&nbsp;{ country.capital.join(', ') }
+        </div>
+        <div>
+          <b>지역 : </b>&nbsp;{ country.region }
+        </div>
+        <div>
+          <b>지도 : </b>&nbsp;<a target="_blank" href={ country.googleMapUrl }>{ country.googleMapUrl }</a>
+        </div>
+      </div>
+    </div>
+  );
 }
