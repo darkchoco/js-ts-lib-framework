b34f78b2e87711f74983d491480e2e0f26620c9c
cacao75 <ih.chon.work@gmail.com>
2025-05-28 15:53:16 +0200

    [ts-ex] drag_and_drop pjt, ProjectState class added (Singleton)

--- ts-ex/drag_and_drop/src/app.ts
+++ ts-ex/drag_and_drop/src/app.ts
@@ -1,3 +1,38 @@
+// Project Status Management
+class ProjectState {
+  private listeners: any[] = [];
+  private projects: any[] = [];
+  private static instance: ProjectState;
+
+  private constructor() {
+  }
+
+  static getInstance() {
+    if (this.instance)
+      return this.instance;
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+
+  addProject(title: string, description: string, numOfPeople: number) {
+    const newProject = {
+      id: Math.random().toString(),
+      title: title,
+      description: description,
+      numOfPeople: numOfPeople,
+    };
+    this.projects.push(newProject);
+    for (const listener of this.listeners)
+      listener(this.projects.slice());
+  }
+
+  addListener(listener: Function) {
+    this.listeners.push(listener);
+  }
+}
+
+const projectState = ProjectState.getInstance();
+
 // Validation
 interface Validatable {
   value: string | number;
@@ -32,6 +67,52 @@ function validate(validatableInput: Validatable) {
   }
 
   return isValid;
+}
+
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+  assignedProjects: any[];
+
+  constructor(private type: 'active' | 'finished') {
+    // 절대로 NULL이 안 될 것이라고 보장할 수 있으므로 ! 을 붙인다.
+    this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    this.assignedProjects = [];
+
+    // 해당 Element의 컨텐츠에 대한 reference를 인수로 전달한다.
+    // true는 해당 reference에 포함된 모든 sub 컨텐츠도 가져오겠다는 의미.
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    projectState.addListener((projects: any[]) => {
+      this.assignedProjects = projects;
+      this.renderProjects();
+    });
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+
+  private renderContent() {
+    this.element.querySelector('ul')!.id = `${this.type}-projects-list`;
+    this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + 'PROJECTS';
+  }
+
+  private renderProjects() {
+    const listElement = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+    for (const projectItem of this.assignedProjects) {
+      const listItem = document.createElement('li');
+      listItem.textContent = projectItem.title;
+      listElement.appendChild(listItem);
+    }
+  }
 }
 
 class ProjectInput {
@@ -103,6 +184,7 @@ class ProjectInput {
       if (Array.isArray(userInput)) {
         const [title, desc, people] = userInput;
         console.log(title, desc, people);
+        projectState.addProject(title, desc, people);
         this.clearInputs();
       }
     });
@@ -114,3 +196,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePjtList = new ProjectList('active');
+const finishedPjtList = new ProjectList('finished');
