4573e2bd1836024fb67c34714cf176cc030c7b38
cacao75 <ih.chon.work@gmail.com>
2025-08-23 23:08:26 +0200

    [mnwd] 04-webapp - update with Promise.

--- mastering-nodejs-web-dev/04-webapp/src/handler.ts
+++ mastering-nodejs-web-dev/04-webapp/src/handler.ts
@@ -1,14 +1,15 @@
 import { IncomingMessage, ServerResponse } from "http";
-import { readFile } from "fs";
+import { readFile } from "fs/promises";
+import { endPromise } from "./promises";
 
-export const handler = (req: IncomingMessage, res: ServerResponse) => {
-  readFile("data.json", (err: Error | null, data: Buffer) => {
-    if (err == null) {
-      res.end(data, () => console.log("File sent"));
-    } else {
-      console.log(`Error: ${err.message}`);
-      res.statusCode = 500;
-      res.end();
-    }
-  });
+export const handler = async (req: IncomingMessage, res: ServerResponse) => {
+  try {
+    const data: Buffer = await readFile("data.json");
+    await endPromise.bind(res)(data);
+    console.log("File sent");
+  } catch (err: any) {
+    console.log(`Error: ${err?.message ?? err}`);
+    res.statusCode = 500;
+    res.end();
+  }
 };
